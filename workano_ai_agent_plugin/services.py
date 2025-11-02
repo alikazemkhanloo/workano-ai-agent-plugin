e980dc869166175893af7be588b256e0c607cd4e
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-11-02 17:28:25 +0330

    only run on specific app

--- workano_ai_agent_plugin/services.py
+++ workano_ai_agent_plugin/services.py
@@ -14,16 +14,16 @@ class AIAgentBusConsumerService():
         print('call entered >>> event: ', event)
         call_id = event.get('call_id')
         application_uuid = event.get('application_uuid')
-        channel = self.ari.channels.get(channelId=call_id)
-        request = {
-            'app': f'wazo-app-{application_uuid}',
-            "external_host": '127.0.0.1:4000',
-            "format":'slin16',
-            "direction":'both'
+        if application_uuid == '1ef1a021-0caa-477c-bc19-764cd65a8a87':
+            channel = self.ari.channels.get(channelId=call_id)
+            request = {
+                'app': f'wazo-app-{application_uuid}',
+                "external_host": '127.0.0.1:4000',
+                "format":'slin16',
+                "direction":'both'
 
-        }
-
-        return channel.externalMedia(**request)
+            }
+            return channel.externalMedia(**request)
 
 
 def build_ai_agent_bus_consumer_service(ari):
